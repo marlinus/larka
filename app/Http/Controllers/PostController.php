84edb9534aeceb4ac8785a25433b27d46e8dabaa
Robert.Nutfullin <Robert.Nutfullin@tatar.ru>
2023-03-25 00:52:09 +0300

    3_change_post_controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller {
 
-		public function __construct() {}
+		public function __construct() {
+			$this->isChanged = false;
+		}
 
 		public function showAll(): string {
 			return '<h1>All posts from database</h1>';
